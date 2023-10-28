e0682396f86a0cad22531668c4f306832e2ef8fd
Victor Assunção <victor.a.schumann@gmail.com>
2023-10-27 22:33:27 -0300

    add footer and login verification

--- public/index.php
+++ public/index.php
@@ -6,4 +6,20 @@ session_start();
 // carregamento das rotas permitidas 
 $rotas_permitidas = require_once __DIR__ . '/../inc/rotas.php';
 
+//definição da rota
+
+$rota = $_GET["rota"] ?? "home";
+
+//verifica se o usuario está logado
+
+if(!isset($_SESSION['usuario'])){
+    $rota =" login";
+}
+
+// se o usuario está logado e tenta entrar no login
+if(isset($_SESSION["usuario"]) && $rota === "login"){
+    $rota = "home";
+}
+
+
 ?>
