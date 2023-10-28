6761896ace706dc78f1dec1f7ae7dd3b83182820
Victor Assunção <victor.a.schumann@gmail.com>
2023-10-28 18:32:46 -0300

    404 error fixed

--- public/index.php
+++ public/index.php
@@ -8,41 +8,41 @@ $rotas_permitidas = require_once __DIR__ . '/../inc/rotas.php';
 
 //definição da rota
 
-$rota = $_GET["rota"] ?? "home";
+$rota = $_GET['rota'] ?? 'home';
 
 //verifica se o usuario está logado
 
 if(!isset($_SESSION['usuario'])){
-    $rota =" login";
+    $rota = "login";
 }
 
 // se o usuario está logado e tenta entrar no login
-if(isset($_SESSION["usuario"]) && $rota === "login"){
-    $rota = "home";
+if(isset($_SESSION['usuario']) && $rota === 'login'){
+    $rota = 'home';
 }
 
 // se a rota não existe
 if(!in_array($rota, $rotas_permitidas)){
-    $rota = "404";
+    $rota = '404';
 }
 
 // preparação da página 
 
 $script = null;
-switch ($variable) {
-    case "404":
-        $script = "404.php";
+switch ($rota) {
+    case '404':
+        $script = '404.php';
         break;
-    case "login":
-        $script = "login.php";
+    case 'login':
+        $script = 'login.php';
         break;
-    case "home":
-        $script = "home.php";
+    case 'home':
+        $script = 'home.php';
         break;
 }
 
 // apresentação da página 
-require_once __DIR__ . "/../inc/header.php.";
+require_once __DIR__ . "/../inc/header.php";
 require_once __DIR__ . "/../scripts/$script";
 require_once __DIR__ . "/../inc/footer.php";
 
