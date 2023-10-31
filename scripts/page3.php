0cb78f7906a6c530062f367af86d770877b81e99
Victor Assunção <victor.a.schumann@gmail.com>
2023-10-31 17:13:41 -0300

    add pages

---
+++ scripts/page3.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/../inc/navbar.php';
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <h4>home</h4>
+            <p class="mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto veniam placeat quos reiciendis dicta blanditiis itaque ducimus pariatur, delectus ex nisi. Minus consectetur reprehenderit soluta quae repellat voluptatum dignissimos illo?
+            Optio iste dolore debitis hic facilis nisi architecto ad omnis quidem itaque quod minima vero saepe unde, aspernatur atque eaque illo sapiente obcaecati ea libero alias est. Est, adipisci ex.
+            Possimus, officia ab hic perspiciatis corrupti necessitatibus accusamus nostrum quo impedit architecto sunt dignissimos non deleniti, modi consequatur dolor soluta repellendus? Perspiciatis id dicta, ex accusamus provident praesentium molestiae ut.
+            Dolore, dignissimos? Nemo iure dolores mollitia sed facere saepe non incidunt corrupti doloribus, ab earum tempore consequuntur libero repellat est culpa? Suscipit laudantium enim placeat eaque sit id, atque laborum!
+            Animi odio, illum at incidunt, perferendis odit quibusdam aspernatur necessitatibus dolores iste dignissimos fuga maxime. Praesentium eum, ipsam rem minus quo ut adipisci atque dolorem, repudiandae facere tempora voluptatibus vero.
+            Saepe enim aperiam repellat quibusdam similique aut? Vitae nostrum quisquam molestias veniam mollitia ea atque facere rerum blanditiis odio sit tempore eum in magni, culpa reiciendis sint nisi, veritatis ratione.
+            Expedita officiis id eligendi culpa eaque fugiat sit? Illum, asperiores quo. Dicta et facilis quas blanditiis assumenda, neque voluptas quidem perspiciatis labore praesentium ab dolore sequi soluta officiis culpa suscipit!
+            Facere, pariatur eligendi voluptas odio natus quod placeat quia. Dolor enim sed impedit at, molestias ad distinctio qui in tempore hic autem amet quisquam, sit mollitia ipsum eos aspernatur officia?
+            Tempora ducimus nobis nihil incidunt ex tenetur soluta blanditiis at quia pariatur reiciendis deserunt unde id nulla quod, nostrum et eius iste atque vero labore. Ratione at unde labore asperiores?
+            Similique nemo sed doloremque ipsa quis? Debitis dolorum corrupti non sunt nam. Maiores totam cumque dignissimos repudiandae, obcaecati recusandae error consequuntur exercitationem. Voluptatem libero dolor beatae excepturi doloribus mollitia tempore.</p>
+        </div>
+    </div>
+</div>
